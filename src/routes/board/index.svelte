8a9199819c6b782021d2fb05fb30671d22355454
whygee-dev <76694551+whygee-dev@users.noreply.github.com>
2022-05-19 22:28:54 +0200

    📦 NEW: protect board route

--- src/routes/board/index.svelte
+++ src/routes/board/index.svelte
@@ -1,1 +1,15 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ session }) => {
+		if (!session.user) {
+			return {
+				status: 302,
+				redirect: '/login'
+			};
+		}
+		return {};
+	};
+</script>
+
 <section>board</section>
