946841a94005a496e29ca2d54af77f2823570a78
whygee-dev <76694551+whygee-dev@users.noreply.github.com>
2022-05-22 08:15:36 +0200

    👌 IMPROVE: local font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,12 +12,7 @@
 
 <svelte:head>
 	<link rel="icon" type="image/svg" href={'logo-sm.svg'} />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
-		rel="stylesheet"
-	/>
+	<link rel="stylesheet" href="/fonts.css" />
 	<meta name="robots" content="noindex, nofollow" />
 </svelte:head>
 
